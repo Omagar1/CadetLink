7534e1a813f7db1c810260f6c2062db01b1f54e1
Omagar1 <89162454+Omagar1@users.noreply.github.com>
2022-03-21 10:23:38 +0000

    Uniform table Works !
     Mess around with styles a bit to make table work,
    
    cant get it in the div main
    style only works if directly on page

--- kitRequest.php
+++ kitRequest.php
@@ -7,30 +7,59 @@
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <script>
-        <?php
-        //echo "<table style='border: solid 1px black;'>";
-        //echo "<tr><th>Id</th><th>NSN</th><th>ItemTypeID</th><th>NumIssued</th><th>NumInStore</th><th>NumReserved</th><th>NumOrdered</th></tr>";
+      <style>
+      /* ----------------------------- table stuff  -----------------------------  */
+        table {
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+        td{
+          width: 150px; 
+          border: 1px solid black; 
+          background-color: rgb(0, 43, 23, 0.938);
+        }
+        th{
+          color: black;
+          background-color: white;
+        }
+        .tableDisplay{
+          width: 100%;
+        }
+        tr:nth-child(even) {
+          background-color: #f2f2f2;
+        }
 
-        class TableRows extends RecursiveIteratorIterator {
-            function __construct($it) {
-                parent::__construct($it, self::LEAVES_ONLY);
-            }
+        th {
+          position: sticky;
+          top: 0; /* Don't forget this, required for the stickiness */
+          box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.4);
+        }
+      </style>
 
-            function current() {
-                return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
-            }
+      <?php
+      session_start();
+      echo "<table class = 'tableDisplay' >";
+      echo "<tr><th>Id</th><th>NSN</th><th>ItemTypeID</th><th>NumIssued</th><th>NumInStore</th><th>NumReserved</th><th>NumOrdered</th></tr>";
 
-            function beginChildren() {
-                echo "<tr>";
-            }
-
-            function endChildren() {
-                echo "</tr>" . "\n";
-            }
+      class TableRows extends RecursiveIteratorIterator {
+          function __construct($it) {
+              parent::__construct($it, self::LEAVES_ONLY);
           }
-        ?>
-        </script>
+
+          function current() {
+              return "<td>" . parent::current(). "</td>";
+          }
+
+          function beginChildren() {
+              echo "<tr>";
+          }
+
+          function endChildren() {
+              echo "</tr>" . "\n";
+          }
+        }
+      ?>
+        
     </head>
 
     <body id = "test">
@@ -40,14 +69,15 @@
       </div>
 
       <div id="navbarDash">
-        <h2 class ="navBarDashTxt"> welcome Sgt sleep paralysis demon</h2>
+        <h2 class ="navBarDashTxt"> welcome <?php echo $_SESSION['rank']. " ";
+            echo $_SESSION['fname']. " ";
+            echo $_SESSION['lname'];?></h2>
         <img class = "profilePic" src="images/SgtWagar.jpg" alt="SgtDefalt" width="auto" height="150">
       </div>
       <div id="container">
         
         <div id="main">
             <h2>Kit Request Page - Work in Progress </h2>
-        <script>
           <?php
           $servername = "2-12.co.uk";
           $username = "jrowden";
@@ -70,9 +100,10 @@
           }
           $conn = null;
           echo "</table>";
-          echo "this ran"
+      
+          //style='width: 150px; border: 1px solid black; background-color: rgb(0, 43, 23, 0.938);'
+          //style='border: solid 1px black;'
           ?>
-          </script>
 
             
         </div>
