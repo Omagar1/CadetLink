2df50ea2b1f79ac863d1da5ea08488e5e84f2405
Omagar1 <89162454+Omagar1@users.noreply.github.com>
2022-05-17 11:05:09 +0100

    First step in adding AJAX/ jQuery

--- SProcess.php
+++ SProcess.php
@@ -12,43 +12,17 @@ require_once "ConnectDB.php";
 
 
 try{
-    if(isset($_POST['sub1NumInStore'])){
-        $itemID = $_POST['sub1NumInStore'];
-        echo "iran ";
-    }elseif(isset($_POST['Plus1NumInStore'])){
-        $itemID = $_POST['Plus1NumInStore'];
-        echo "iran2" ;
+    $Operation = $_REQUEST["ItemID"];
+    $itemID = $_REQUEST["ItemID"];
+    
+    if($Operation == "-"){
+        $qry = "UPDATE items SET NumInStore = NumInStore - 1  WHERE ID = ? AND NumInStore > 0 ;";
+    }elseif($Operation == "+"){
+        $qry = "UPDATE items SET NumInStore = NumInStore + 1 WHERE ID = ?;";  
     }
-    else{
-        header("location: stock.php");
-    }
-    // getting currant data  
-    $qry = "SELECT NumInStore FROM items WHERE ID = ?;" ;
     $stmt = $conn->prepare($qry);
     $stmt->execute([$itemID]);
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    //var_dump($result);
-
-    $oldNumInStore = $result["NumInStore"];
-    if(isset($_POST['sub1NumInStore'])){
-        if ($oldNumInStore != 0){
-        $newNumInStore  = $oldNumInStore - 1;
-        }
-        else{
-        
-        }
-        
-    }elseif(isset($_POST['Plus1NumInStore'])){
-        $newNumInStore  = $oldNumInStore + 1;  
-        
-    }
-    
-    // updating items table 
-    
-    $qry = "UPDATE items SET NumInStore =? WHERE ID = ?;";
-    $stmt = $conn->prepare($qry);
-    $stmt->execute([$newNumInStore, $itemID]);
-    header("location: Stock.php");
+    //header("location: Stock.php");
 } catch (PDOException $e) {
     echo "Error : ".$e->getMessage();
 }
