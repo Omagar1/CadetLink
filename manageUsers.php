66878334cdfc955471e20e5a7dd6e553e73de5db
Omagar1 <89162454+Omagar1@users.noreply.github.com>
2022-05-26 10:15:09 +0100

    Sorting System is working
    also commented

--- manageUsers.php
+++ manageUsers.php
@@ -34,6 +34,7 @@
       $_SESSION['previous'] = basename($_SERVER['PHP_SELF']);
 
 //----------------------------------------Main Code----------------------------------------
+
 // initialising colum arrays
 $IDArr = [];
 $CnumArr = [];
@@ -42,75 +43,53 @@ $fnameArr = [];
 $lnameArr = [];
 $troopArr = [];
 
-
+// ---------------------------------------- sorting System ----------------------------------------
 if (isset($_POST['find'])){
+  // initializing Variables 
+  $sql = "SELECT * FROM users WHERE ";
   $troop = $_POST["troop"];
   $rank = trim($_POST["rank"]);
   $fname = trim($_POST["fname"]);
   $lname = trim($_POST["lname"]);
-  
-  //with all
-  if($rank != "" and $fname != "" and $lname != "" and $troop != 0){
-    $sql = "SELECT * FROM users WHERE ID = ? AND `rank` = ? AND `fname` = ? AND `lname` = ? ;";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$troop, $rank, $fname, $lname]);
-    $count = $stmt->rowCount();
-  //with troop
-  }elseif ($troop != 0 /* */ and $rank == "" and $fname == "" and $lname == ""){
-    $sql = "SELECT * FROM users WHERE ID = ?;";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$troop]);
-    $count = $stmt->rowCount();
-  }elseif($rank != "" and $fname != "" and $troop != 0 /* */ and $lname == ""){
-    $sql = "SELECT * FROM users WHERE ID = ? AND `rank` = ? AND `fname` = ?;";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$troop, $rank, $fname]);
-    $count = $stmt->rowCount();
-  }elseif($rank != "" and $troop != 0 /* */and $fname == "" and $lname == ""){
-    $sql = "SELECT * FROM users WHERE ID = ? AND `rank` = ?;";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$troop, $rank]);
-    $count = $stmt->rowCount();
-  // with rank and without troop
-  }elseif($rank != "" and $fname != "" and $lname != "" and /* */ $troop == 0){
-    $sql = "SELECT * FROM users WHERE `rank` = ? AND `fname` = ? AND `lname` = ? ;";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$rank, $fname, $lname]);
-    $count = $stmt->rowCount();
-  }elseif($rank != "" and $fname != "" /* */and $lname == "" and $troop == 0){
-    $sql = "SELECT * FROM users WHERE `rank` = ? AND `fname` = ?;";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$rank, $fname]);
-    $count = $stmt->rowCount();
-  }elseif($rank != ""  and /* */ $fname == "" and $lname == "" and $troop == 0){
-    $sql = "SELECT * FROM users WHERE `rank` = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$rank,]);
-    $count = $stmt->rowCount();
-  // with fname and without troop and Rank
-  }elseif($fname != "" and $lname != "" and /* */ $rank == 1  and $troop == 0){
-    $sql = "SELECT * FROM users WHERE `fname` = ? AND `lname` = ? ;";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$fname, $lname]);
-    $count = $stmt->rowCount();
-  }elseif($fname != "" /* */and $rank == "" and $lname == "" and $troop == 0){
-    $sql = "SELECT * FROM users WHERE`fname` = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$fname,]);
-    $count = $stmt->rowCount();
-  // with lname 
-  }elseif($lname != ""  /* */ and $rank == "" and $fname == ""  and $troop == 0){
-    $sql = "SELECT * FROM users WHERE `lname` = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$rank,]);
-    $count = $stmt->rowCount();
+  //testing 
+  // echo $_POST["troop"];
+  // echo $_POST["rank"];
+  // echo $_POST["fname"];
+  // echo $_POST["lname"];
+  //SQL builder: adds the parts of the Sql only if it is filtered 
+  if($troop != "No Filter"){
+    $sql = $sql . "troop = ? AND ";
+    //echo "<br> I Ran 1"; //test
   }else{
-    $sql = "SELECT * FROM users;";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-    $count = $stmt->rowCount();
-    //echo "I ran 4 <br>";
+    $sql = $sql . "troop != ? AND "; // instead of not adding a bit of sql to $sql we add this line so I don't have to anything special for the Execute function
+    //echo "<br> I Ran 2"; //test
   }
+  if ($rank!= "No Filter"){;
+    $sql = $sql . "rank = ? AND ";
+  }else{
+    $sql = $sql . "rank != ? AND ";
+  }
+  if ($fname != ""){
+    $sql = $sql . "fname = ? AND ";
+  }else{
+    $sql = $sql . "fname != ? AND ";
+  }
+  if ($lname != ""){
+    $sql = $sql . "lname = ? AND "; 
+  }else{
+    $sql = $sql . "lname != ? AND ";
+  }
+  echo "<br> sql1: ".  $sql;
+  $lastAnd = strpos($sql, "AND", -4);
+  echo "<br> lastAnd: ". $lastAnd;
+  //$lenSql = strlen($sql);
+  $sql = substr_replace($sql,"",$lastAnd,3);
+  $sql = $sql . ";";
+  echo"<br> sql2: ".  $sql;
+  
+  $stmt = $conn->prepare($sql);
+  $stmt->execute([$troop, $rank, $fname, $lname]);
+  $count = $stmt->rowCount();
 }else{
 // Qry to find all users 
 $sql = "SELECT * FROM users;";
@@ -120,6 +99,7 @@ $count = $stmt->rowCount();
 //echo "I ran 2 <br>";
 }
 $empty = 0;
+// Checks if the result of the qry is 0 
   if ($count == 0){
     $empty = 1;
   }else{
@@ -127,7 +107,7 @@ $empty = 0;
   }
 
 
-// add the value in Each row's data to their respective colums Arrays this is done so data can be modified prior to display 
+// add the value in Each row's data to their respective columns Arrays this is done so data can be modified prior to display 
 while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
   //echo $row['ID'] . "<br>"; test
   array_push($IDArr,$row['ID']);
@@ -147,11 +127,11 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
     </div>
 
     <div id="navbarDash">
-      <h2 class ="navBarDashTxt"> welcome <?php echo $_SESSION['rank']. " ";
+      <h2 class ="navBarDashTxt"> welcome <?php echo $_SESSION['rank']. " "; // print out the users  rank and Name 
           echo $_SESSION['fname']. " ";
           echo $_SESSION['lname'];?></h2>
       <img class = "profilePic" src="images/<?php echo $_SESSION['profilePicURL'];?>" alt="SgtDefalt" width="auto" height="150">
-      <form action ="<?php
+      <form action ="<?php // back button - checks if CFAV and what mainPage it should be Sent to 
       if($_SESSION['troop']=="CFAV"){
         echo "adminMainPage.php";
       }else{
@@ -166,7 +146,8 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
       <div id="main">
           <h2>User Management System - Work in Progress </h2>
           <fieldset>
-          <?php if (isset($_SESSION["msg"]) != ""){
+          <?php // error message system
+          if (isset($_SESSION["msg"]) != ""){
                 echo $_SESSION['msg'];
                 }else{
                 
@@ -175,13 +156,17 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
             <form action = "manageUsers.php" method="post">
             <label for="troop">Troop</label><br>
             <select id="troop" name="troop">
-              <option value="0">No Filter</option>
-              <?php
+              <option value="No Filter">No Filter</option>
+              <?php // qry to find the troop name then display them as a select option 
                 $sql = "SELECT * FROM troops;";
                 $stmt = $conn->prepare($sql);
                 $stmt->execute();
                 while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-                  echo "<option value=".$row["troopName"].">".$row["troopName"]."</option>";
+                  if ($row["troopName"] == $troop){
+                    echo "<option value='".$row["troopName"]."' selected>".$row["troopName"]."</option>";
+                  }else{
+                    echo "<option value='".$row["troopName"]."'>".$row["troopName"]."</option>";
+                  }
                 }
                 ?>
               </select><br>
@@ -189,19 +174,30 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
               
               ?>
               <label >Rank and Name</label><br>
-              <input type="text" id="rank" name="rank" placeholder = "Rank" value="<?php 
-              if (isset($rank)){
-                echo $rank; 
-              }else{
-                echo " "; 
-              } ?>">
+              <select id="rank" name="rank">
+                <option value="No Filter">No Filter</option>
+                <?php // qry to find the ranks then display them as a select option
+                $sql = "SELECT * FROM ranks;";
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+                while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                  if ($row["rank"] == $rank){
+                    echo "<option value='".$row["rank"]."'selected>".$row["rank"]."</option>";
+                  }else{
+                    echo "<option value='".$row["rank"]."'>".$row["rank"]."</option>";
+                  }
+                }
+              ?>
+              </select><br></td>
               <input type="text" id="fname" name="fname" placeholder = "First Name" value="<?php
+              // checks if the user has searched for something in this input box if so displays their input 
               if (isset($fname)){
                 echo $fname; 
               }else{
                 echo " "; 
               } ?>">
               <input type="text" id="lname" name="lname" placeholder = "Last Name" value="<?php
+              // checks if the user has searched for something in this input box if so displays their input
                if (isset($lname)){
                 echo $lname; 
                }else{
@@ -215,7 +211,7 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
               <table class = "tableDisplay">
                 <tr>
                   <th>ID</th>
-                  <th>Cnum</th>
+                  <th>Cadet Number</th>
                   <th>Rank</th>
                   <th>first name</th>
                   <th>last name</th>
@@ -234,7 +230,7 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
                   ////var_dump($IDArr); //test
                   // echo $empty; // test
               
-                // actuly what displays 
+                // table of users that displays 
                   while($loop < count($IDArr)){ 
                   echo "<tr>";
                       echo "<td>" .  $IDArr[$loop] . "</td>" ;
@@ -243,16 +239,19 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
                       echo "<td>" .  $fnameArr[$loop] . "</td>"; 
                       echo "<td>" .  $lnameArr[$loop] . "</td>";
                       echo "<td>" .  $troopArr[$loop]. "</td>";
+                      // Reset Password button
                       echo "<td> <form method='post' action ='resetPWord.php'>
                       <input type='hidden' id = 'resetPWord' name='resetPWord' value=' $IDArr[$loop] '/>
                       <input type='submit' name='RP' value='Reset Password'/>
                       </form>";
+                      // edit User button
                       echo "<td>
                       <form method='post' action ='editUser.php'>
                       <input type='hidden' id = 'editUser' name='editUser' value=' $IDArr[$loop] '/>
                       <input type='submit' name='eU' value='edit'/>
                       </form>
                       </td>";
+                      // delete user button 
                       echo "<td>
                       <form method='post' action ='deleteUser.php'>
                       <input type='hidden' id = 'Xdata' name='Xdata' value=' $IDArr[$loop] '/>
@@ -261,7 +260,7 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
                       </td>";
                   echo "</tr>";
                   
-                  $loop = $loop + 1;
+                  $loop = $loop + 1;// increments the loop index
                   }
                   ?>
 
